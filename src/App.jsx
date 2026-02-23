6dd976e6f2d755253683b9b325e6f2ac2b3bb8a0
Voodoo30fps <voodoo30fps@gmail.com>
2026-02-23 20:29:47 +0200

    rendering content conditionally - tab buttons and tab content

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 import { CORE_CONCEPTS } from './data.js';
+import { EXAMPLES } from './data.js';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState(null);
+
   function handleSelect(selectedButton) {
     // selectedButton = 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -32,6 +51,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
