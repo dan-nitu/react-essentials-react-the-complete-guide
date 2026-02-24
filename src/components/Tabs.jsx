cfff9d5a2d2d84e2326b8443c1278147f0d6eca5
Voodoo30fps <voodoo30fps@gmail.com>
2026-02-24 16:15:26 +0200

    setting default prop values

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,8 +1,10 @@
-export default function Tabs({ children, buttons, ButtonsContainer }) {
+export default function Tabs({ children, buttons, ButtonsContainer = 'menu' }) {
   // buttons = slot - for the tab buttons
 
   // ButtonsContainer = must start with capital letter - used as custom component
   // the alternative is to start with lower case but remap it (for buttonsContainer it would be const buttonsContainer = ButtonsContainer)
+
+  // ButtonsContainer = 'menu' - default value for the ButtonsContainer prop, if not provided it will use 'menu' as the default container for the buttons
   return (
     <>
       <ButtonsContainer>{buttons}</ButtonsContainer>
